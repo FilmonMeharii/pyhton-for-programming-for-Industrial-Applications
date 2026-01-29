a1131a73b7b80f7020052b5b2d2051f23d07538d
Filmon Mehari <filmonmehari08@gmail.com>
2026-01-30 00:14:27 +0100

    Add Computing.py and expand greeting.py functionality
    Introduced Computing.py to perform simple arithmetic and display pi. Enhanced greeting.py to collect and display additional user information (age, weight, height), and added examples of variable assignment and input handling.

---
+++ lectures/Computing.py
@@ -0,0 +1,7 @@
+answer = input('pick a number! ')
+user_num = float(answer)
+third = user_num/3
+print('here is a third:', third)
+
+pi = 3.141592653589793
+print(f'pi = {pi:.2f}')
--- lectures/greeting.py
+++ lectures/greeting.py
@@ -1,2 +1,16 @@
-name = input('what is your name?')
-print('Greetings ', name)
+name = input('what is your name? ')
+print('Greetings', name, '!')
+
+age = input('How old are you? ')
+weight = input('What is your weight (in kg)? ')
+height = input('What is your height (in cm)? ')
+print('My name is',name, 'I am', age, 'years old, and my weight is', weight, 'kg and my height is' , height, ' cm.')
+
+length, width = 4.5, 6.0
+print(length)
+print(width)
+print(length, end =' ')
+print(width)
+
+ans = input('what is your age?')
+age= int(ans)
