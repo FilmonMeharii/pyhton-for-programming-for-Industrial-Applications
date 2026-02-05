5a2db72e564a7281f9e829c288e8e00e26a8227d
Filmon Mehari <filmonmehari08@gmail.com>
2026-02-05 14:07:57 +0100

    Update sequences_membership.py

--- sequesnces/sequences_membership.py
+++ sequesnces/sequences_membership.py
@@ -5,3 +5,11 @@ print(f'There is an x in {s}: {"x" in s}')
 
 
 print('old' in 'folded')
+
+s ='Some sensetive subject'
+for c in s:
+    if c in 'aeiouy':
+        print(c, end=' ')
+    else:
+        print(c, end='')
+        
